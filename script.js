77b781bb8ef92dbe89ecd307dae7e1065584a8b3
SrushtiMulik <127543398+SrushtiMulik@users.noreply.github.com>
2024-10-06 17:43:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onscroll=function(){
+    var navbar=document.getElementById("navbar");
+    if(window.pageYOffset>100){
+        navbar.classList.add("scrolled");
+
+    }else{
+        navbar.classList.remove("scrolled");
+
+    }
+};
